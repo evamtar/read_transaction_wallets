bbbe99637b1fc70030c716a1d073bb8ea868034d
Evandro de Almeida Tartari <evandro_tartari@hotmail.com>
2024-01-16 10:20:52 -0300

    - Mapeado as classes de response das API's de token da birdeye

--- Transactions/SyncronizationBot.Domain/Model/CrossCutting/Birdeye/TokenCreation/Response/TokenCreationResponse.cs
+++ Transactions/SyncronizationBot.Domain/Model/CrossCutting/Birdeye/TokenCreation/Response/TokenCreationResponse.cs
@@ -4,5 +4,16 @@ namespace SyncronizationBot.Domain.Model.CrossCutting.Birdeye.TokenCreation.Resp
 {
     public class TokenCreationResponse
     {
+        public ResultData? Data { get; set; }
+        public bool Success { get; set; }
+
+        public class ResultData
+        {
+            public string? TxHash { get; set; }
+            public decimal? Slot { get; set; }
+            public string? TokenAddress { get; set; }
+            public decimal? Decimals { get; set; }
+            public string? Owner { get; set; }
+        }
     }
 }
