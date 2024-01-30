64cad6398eb0d6bcd3c6421316e2a97f0d1fb931
Evandro de Almeida Tartari <evandro.a.tartari@gmail.com>
2024-01-30 13:57:52 -0300

    - Ajustes e correções para problemas encontrados em teste

--- Transactions/SyncronizationBot.Application/Handlers/SolanaFM/RecoverySaveBalanceSFMCommandHandler.cs
+++ Transactions/SyncronizationBot.Application/Handlers/SolanaFM/RecoverySaveBalanceSFMCommandHandler.cs
@@ -1,15 +1,11 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Options;
 using SyncronizationBot.Application.Commands;
-using SyncronizationBot.Application.Commands.Birdeye;
 using SyncronizationBot.Application.Commands.SolanaFM;
 using SyncronizationBot.Application.Handlers.Base;
 using SyncronizationBot.Application.Response;
-using SyncronizationBot.Application.Response.Birdeye;
 using SyncronizationBot.Application.Response.SolanaFM;
 using SyncronizationBot.Domain.Model.Configs;
-using SyncronizationBot.Domain.Model.CrossCutting.Birdeye.WalletPortifolio.Request;
-using SyncronizationBot.Domain.Model.CrossCutting.Birdeye.WalletPortifolio.Response;
 using SyncronizationBot.Domain.Model.CrossCutting.Solanafm.AccountInfo.Request;
 using SyncronizationBot.Domain.Model.CrossCutting.Solanafm.AccountInfo.Response;
 using SyncronizationBot.Domain.Model.CrossCutting.Solanafm.TokensAccountsByOwner.Request;
@@ -17,7 +13,6 @@ using SyncronizationBot.Domain.Model.CrossCutting.Solanafm.TokensAccountsByOwner
 using SyncronizationBot.Domain.Model.Database;
 using SyncronizationBot.Domain.Model.Enum;
 using SyncronizationBot.Domain.Repository;
-using SyncronizationBot.Domain.Service.CrossCutting.Birdeye;
 using SyncronizationBot.Domain.Service.CrossCutting.Solanafm;
 
 namespace SyncronizationBot.Application.Handlers.SolanaFM
@@ -43,14 +38,11 @@ namespace SyncronizationBot.Application.Handlers.SolanaFM
         }
         public async Task<RecoverySaveBalanceSFMCommandResponse> Handle(RecoverySaveBalanceSFMCommand request, CancellationToken cancellationToken)
         {
-            var token = (RecoverySaveTokenCommandResponse)null!;
-            token = await this._mediator.Send(new RecoverySaveTokenCommand { TokenHash = "So11111111111111111111111111111111111111112" });
-            var checkedExists = await this._walletBalanceRepository.FindFirstOrDefault(x => x.IdWallet == request.WalletId && x.IdToken == token.TokenId);
-            if (checkedExists == null)
-            {
-                var accountInfo = await this._accountInfoService.ExecuteRecoveryAccountInfoAsync(new AccountInfoRequest { WalletHash = request.WalletHash });
-                await this.SaveBalance(request, token, accountInfo, DateTime.Now);
-            }
+            // Recovery Total Amount in SOL
+            var accountInfo = await this._accountInfoService.ExecuteRecoveryAccountInfoAsync(new AccountInfoRequest { WalletHash = request.WalletHash });
+            await this.SaveBalance(request, accountInfo, DateTime.Now);
+
+            // Recovery Total Amount in another tokens
             var tokensAccountsByOwners = await this._tokensAccountsByOwnerService.ExecuteRecoveryTokensAccountsByOwnerAsync(new TokensAccountsByOwnerRequest { WalletPublicKeyHash = request.WalletHash });
             var dateLoadBalance = DateTime.Now;
             if (tokensAccountsByOwners.Any())
@@ -61,8 +53,7 @@ namespace SyncronizationBot.Application.Handlers.SolanaFM
                     {
                         foreach (var tokenResultResponse in tokenAccountsByOwner.Result.Value)
                         {
-                            token = await this._mediator.Send(new RecoverySaveTokenCommand { TokenHash = tokenResultResponse.Pubkey });
-                            await this.SaveBalance(request, token, tokenResultResponse, dateLoadBalance);
+                            await this.SaveBalance(request, tokenResultResponse, dateLoadBalance);
                         }
                     }
                 }
@@ -70,9 +61,10 @@ namespace SyncronizationBot.Application.Handlers.SolanaFM
             return new RecoverySaveBalanceSFMCommandResponse { DateLoadBalance = (base.IsSaveBalance() ?? false)? dateLoadBalance : null };
         }
 
-        private async Task SaveBalance(RecoverySaveBalanceSFMCommand request, RecoverySaveTokenCommandResponse token, AccountInfoResponse accountInfo, DateTime? dateLoadBalance)
+        private async Task SaveBalance(RecoverySaveBalanceSFMCommand request, AccountInfoResponse accountInfo, DateTime? dateLoadBalance)
         {
             var balance = (WalletBalance)null!;
+            var token = await this._mediator.Send(new RecoverySaveTokenCommand { TokenHash = "So11111111111111111111111111111111111111112" });
             if (accountInfo != null && accountInfo.Result?.Value?.Lamports > 0)
             {
                 if (base.IsSaveBalance() ?? false)
@@ -129,9 +121,10 @@ namespace SyncronizationBot.Application.Handlers.SolanaFM
             }
         }
 
-        private async Task SaveBalance(RecoverySaveBalanceSFMCommand request, RecoverySaveTokenCommandResponse token, TokenAccountResponse tokenAccount, DateTime? dateLoadBalance)
+        private async Task SaveBalance(RecoverySaveBalanceSFMCommand request, TokenAccountResponse tokenAccount, DateTime? dateLoadBalance)
         {
             var balance = (WalletBalance)null!;
+            var token = await this._mediator.Send(new RecoverySaveTokenCommand { TokenHash = tokenAccount.Pubkey });
             if (tokenAccount != null && tokenAccount.Account?.Data?.Parsed?.Info?.TokenAmount!= null)
             {
                 if (base.IsSaveBalance() ?? false)
@@ -173,7 +166,7 @@ namespace SyncronizationBot.Application.Handlers.SolanaFM
                         IdWalletBalance = null,
                         IdWallet = request.WalletId,
                         IdToken = token?.TokenId,
-                        TokenHash = token?.Hash,
+                        TokenHash = tokenAccount.Pubkey,
                         OldQuantity = (decimal?)0,
                         NewQuantity = quantity,
                         RequestQuantity = quantity,
