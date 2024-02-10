fcaa718acb6b57bc1c42df5a475e4de6ba14187b
Evandro de Almeida Tartari <evandro.a.tartari@gmail.com>
2024-02-10 20:17:40 -0300

    - Ajustes

---
+++ DB/Query's adicionais e dados adicionais/LIMPAR_DADOS_INUTEIS.sql
@@ -0,0 +1,6 @@
+-- VALIDAR SE É APENAS LIXO
+SELECT * FROM TransactionNotMapped WHERE StackTrace IS NULL
+--LIMPAR DADOS INUTEIS
+DELETE FROM PublishMessage WHERE ItWasPublished = 1
+DELETE FROM TelegramMessage WHERE IsDeleted = 1
+DELETE FROM TransactionNotMapped WHERE StackTrace IS NULL
