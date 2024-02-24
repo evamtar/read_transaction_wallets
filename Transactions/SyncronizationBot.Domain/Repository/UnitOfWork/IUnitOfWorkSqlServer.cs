d54984f85196ae9f915871ac147db61996a996dd
Evandro de Almeida Tartari <evandro.a.tartari@gmail.com>
2024-02-24 18:19:56 -0300

    - Correção de contextos para devolver a propriedade referida

--- Transactions/SyncronizationBot.Domain/Repository/UnitOfWork/IUnitOfWorkSqlServer.cs
+++ Transactions/SyncronizationBot.Domain/Repository/UnitOfWork/IUnitOfWorkSqlServer.cs
@@ -6,12 +6,31 @@ namespace SyncronizationBot.Domain.Repository.UnitOfWork
 {
     public interface IUnitOfWorkSqlServer : IDisposable
     {
-        ITokenRepository TokenRepository { get; }
-        IRunTimeControllerRepository RunTimeControllerRepository { get; }
+        IWalletRepository WalletRepository { get; }
         IWalletBalanceRepository WalletBalanceRepository { get; }
         IWalletBalanceHistoryRepository WalletBalanceHistoryRepository { get; }
-        IWalletRepository WalletRepository { get; }
-        
+        ITypeOperationRepository TypeOperationRepository { get; }
+        ITransactionTokenRepository TransactionTokenRepository { get; }
+        ITransactionRPCRecoveryRepository TransactionRPCRecoveryRepository { get; }
+        ITransactionNotMappedRepository TransactionNotMappedRepository { get; }
+        ITransactionsRepository TransactionsRepository { get; }
+        ITokenSecurityRepository TokenSecurityRepository { get; }
+        ITokenPriceHistoryRepository TokenPriceHistoryRepository { get; }
+        ITokenRepository TokenRepository { get; }
+        ITokenAlphaWalletRepository TokenAlphaWalletRepository { get; }
+        ITokenAlphaWalletHistoryRepository TokenAlphaWalletHistoryRepository { get; }
+        ITokenAlphaRepository TokenAlphaRepository { get; }
+        ITokenAlphaHistoryRepository TokenAlphaHistoryRepository { get; }
+        ITokenAlphaConfigurationRepository TokenAlphaConfigurationRepository { get; }
+        ITelegramMessageRepository TelegramMessageRepository { get; }
+        ITelegramChannelRepository TelegramChannelRepository { get; }
+        IRunTimeControllerRepository RunTimeControllerRepository { get; }
+        IClassWalletRepository ClassWalletRepository { get; }
+        IAlertPriceRepository AlertPriceRepository { get; }
+        IAlertParameterRepository AlertParameterRepository { get; }
+        IAlertInformationRepository AlertInformationRepository { get; }
+        IAlertConfigurationRepository AlertConfigurationRepository { get; }
+
         Task SaveChangesAsync();
         void SaveChanges();
     }
--- Transactions/SyncronizationBot.Domain/Repository/UnitOfWork/IUnitOfWorkSqlServerReadyOnly.cs
+++ Transactions/SyncronizationBot.Domain/Repository/UnitOfWork/IUnitOfWorkSqlServerReadyOnly.cs
@@ -1,9 +1,34 @@
 ﻿
 
+using SyncronizationBot.Domain.Repository.Base.Interfaces;
+
 namespace SyncronizationBot.Domain.Repository.UnitOfWork
 {
     public interface IUnitOfWorkSqlServerReadyOnly : IDisposable
     {
-        
+        IWalletRepository WalletRepository { get; }
+        IWalletBalanceRepository WalletBalanceRepository { get; }
+        IWalletBalanceHistoryRepository WalletBalanceHistoryRepository { get; }
+        ITypeOperationRepository TypeOperationRepository { get; }
+        ITransactionTokenRepository TransactionTokenRepository { get; }
+        ITransactionRPCRecoveryRepository TransactionRPCRecoveryRepository { get; }
+        ITransactionNotMappedRepository TransactionNotMappedRepository { get; }
+        ITransactionsRepository TransactionsRepository { get; }
+        ITokenSecurityRepository TokenSecurityRepository { get; }
+        ITokenPriceHistoryRepository TokenPriceHistoryRepository { get; }
+        ITokenRepository TokenRepository { get; }
+        ITokenAlphaWalletRepository TokenAlphaWalletRepository { get; }
+        ITokenAlphaWalletHistoryRepository TokenAlphaWalletHistoryRepository { get; }
+        ITokenAlphaRepository TokenAlphaRepository { get; }
+        ITokenAlphaHistoryRepository TokenAlphaHistoryRepository { get; }
+        ITokenAlphaConfigurationRepository TokenAlphaConfigurationRepository { get; }
+        ITelegramMessageRepository TelegramMessageRepository { get; }
+        ITelegramChannelRepository TelegramChannelRepository { get; }
+        IRunTimeControllerRepository RunTimeControllerRepository { get; }
+        IClassWalletRepository ClassWalletRepository { get; }
+        IAlertPriceRepository AlertPriceRepository { get; }
+        IAlertParameterRepository AlertParameterRepository { get; }
+        IAlertInformationRepository AlertInformationRepository { get; }
+        IAlertConfigurationRepository AlertConfigurationRepository { get; }
     }
 }
